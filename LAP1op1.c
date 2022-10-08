011577d4f84704c464b51ec2417fc136d2a04d69
Abdullah <113207637+Pokerfacez1@users.noreply.github.com>
2022-10-08 19:59:44 +0300

    first commit

---
+++ LAP1op1.c
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+void trig_func()
+{
+	double x;
+	double *xx =  &x;
+	cout<<"Enter any numper you need to sort : "<<endl;
+	cin>>x;
+	cout<<"Sin value of x = "<<sin(x)<<endl;
+	cout<<"Cos value of x = "<<cos(x)<<endl;
+	cout<<"tan value of x = "<<tan(x)<<endl;
+	          
+	          
+}
+
+int main()
+{
+	trig_func();
+
+	return 0 ;
+}
